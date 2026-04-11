4b42363e7817b15b077dd757e3e6dbfbb5b9b920
Nader PJR <pjrnader@gmail.com>
2026-04-11 19:38:50 +0430

    icons added to share section

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,6 +1,28 @@
+import Image from "next/image";
+
 const Share = () => {
     return (
-        <div>Share</div>
+        <div className="p-4 flex gap-4">
+            {/* Avatar  */}
+            <div className="relative w-10 h-10 rounded-full overflow-hidden">
+                <Image src="/general/avatar.png" alt="avatar" width={100} height={100} />
+            </div>
+            {/* Others  */}
+            <div className="flex-1 flex flex-col gap-4">
+                <input type="text" placeholder="What is happening?!" />
+                <div className=" flex items-center justify-between gap-4 flex-wrap">
+                    <div className="flex gap-4 felx-wrap">
+                        <Image src="/icons/image.svg" alt="image" width={20} height={20} className="cursor-pointer" />
+                        <Image src="/icons/gif.svg" alt="image" width={20} height={20} className="cursor-pointer" />
+                        <Image src="/icons/poll.svg" alt="image" width={20} height={20} className="cursor-pointer" />
+                        <Image src="/icons/emoji.svg" alt="image" width={20} height={20} className="cursor-pointer" />
+                        <Image src="/icons/schedule.svg" alt="image" width={20} height={20} className="cursor-pointer" />
+                        <Image src="/icons/location.svg" alt="image" width={20} height={20} className="cursor-pointer" />
+                    </div>
+                    <button className="bg-white text-black font-bold rounded-full py-2 px-4">Post</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
