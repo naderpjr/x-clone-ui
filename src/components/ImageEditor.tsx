25abb73dc909e40b8ec52bff9d193db69e42bd63
Nader PJR <pjrnader@gmail.com>
2026-04-11 21:01:51 +0430

    adding ui to image editor

--- src/components/ImageEditor.tsx
+++ src/components/ImageEditor.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const ImageEditor = ({
     onClose,
     previewURL,
@@ -16,7 +18,19 @@ const ImageEditor = ({
     return (
         <div className="fixed w-screen h-screen left-0 top-0 bg-black bg-opacity-75 z-10 flex items-center justify-center">
             <div className="bg-black rounded-xl p-12 flex flex-col gap-4">
-                Test
+                {/* Top section  */}
+                <div className="flex items-center justify-between">
+                    <svg className="cursor-pointer" width={32} viewBox="0 0 24 24" onClick={onClose}>
+                        <path fill="#e7e9ea" d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z" />
+                    </svg>
+                    <h1 className="font-bold text-xl">Media Settings</h1>
+                </div>
+                {/* Image container  */}
+                <div className="w-[600px] h-[600px] flex items-center">
+                    <Image src={previewURL} alt="" width={600} height={600} className={`w-full ${settings.type === "original" ? "h-full object-contain" : settings.type === "square" ? "aspect-square object-cover" : "aspect-video object-cover"}`} />
+                </div>
+                {/* settings  */}
+                <div className=""></div>
             </div>
         </div>
     )
