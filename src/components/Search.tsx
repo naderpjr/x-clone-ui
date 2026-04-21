3d8bc8faf41351ba95695a0e87cbcdcdd021ce45
Nader PJR <pjrnader@gmail.com>
2026-04-21 19:33:46 +0430

    popular tags designed and searchbar added

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,11 @@
+import Image from "next/image";
+
 const Search = () => {
     return (
-        <div>Search</div>
+        <div className="bg-inputGray py-2 px-4 flex items-center gap-4 rounded-full">
+            <Image src="icons/explore.svg" alt="search" width={16} height={16} />
+            <input type="text" placeholder="Search" className="bg-transparent outline-none placeholder:text-textGray" />
+        </div>
     )
 }
 
