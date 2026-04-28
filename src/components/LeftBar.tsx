ac5450298566acf5a6b687bfe68114445c8c25dd
Nader PJR <pjrnader@gmail.com>
2026-04-28 22:08:05 +0430

    adding parallel routing to post function

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -87,10 +87,10 @@ const LeftBar = () => {
 
                 {/* button  */}
 
-                <Link href="/" className="bg-white text-black rounded-full w-12 h-12 flex items-center justify-center xxl:hidden">
+                <Link href="/compose/post" className="bg-white text-black rounded-full w-12 h-12 flex items-center justify-center xxl:hidden">
                     <Image src="icons/post.svg" alt="new post" width={24} height={24} />
                 </Link>
-                <Link href="/" className="hidden xxl:block bg-white text-black rounded-full font-bold py-2 px-20">
+                <Link href="/compose/post" className="hidden xxl:block bg-white text-black rounded-full font-bold py-2 px-20">
                     Post
                 </Link>
 
